37d7d0c899681bf2318321776e7979421f75c5dc
David Ruda <davidrth@seznam.cz>
2023-05-08 09:00:27 +0200

    First version of the evaluator

--- src/main/java/cz/cuni/mff/java/Main.java
+++ src/main/java/cz/cuni/mff/java/Main.java
@@ -1,7 +1,36 @@
 package cz.cuni.mff.java;
 
+import cz.cuni.mff.java.evaluator.HandEvaluator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Community cards
+        int a = 7 * 4 + 0; // 9c
+        int b = 2 * 4 + 0; // 4c
+        int c = 2 * 4 + 3; // 4s
+        int d = 7 * 4 + 1; // 9d
+        int e = 2 * 4 + 2; // 4h
+
+        // Player 1
+        int f = 10 * 4 + 0; // Qc
+        int g = 4 * 4 + 0; // 6c
+
+        // Player 2
+        int h = 0 * 4 + 0; // 2c
+        int i = 7 * 4 + 2; // 9h
+
+        // Evaluating the hand of player 1
+        int rank1 = HandEvaluator.evaluate7Cards(a, b, c, d, e, f, g);
+        // Evaluating the hand of player 2
+        int rank2 = HandEvaluator.evaluate7Cards(a, b, c, d, e, h, i);
+
+        assert(rank1 == 292);
+        assert(rank2 == 236);
+
+        System.out.printf("The rank of the hand in player 1 is %d\n", rank1); // expected 292
+        System.out.printf("The rank of the hand in player 2 is %d\n", rank2); // expected 236
+        System.out.println("Player 2 has a stronger hand");
     }
 }
---
+++ src/main/java/cz/cuni/mff/java/evaluator/HandEvaluator.java
@@ -0,0 +1,123 @@
+package cz.cuni.mff.java.evaluator;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public final class HandEvaluator {
+    private HandEvaluator() {
+
+    }
+
+    public static short evaluate7Cards(int a, int b, int c, int d, int e, int f, int g) {
+        int suitHash = 0;
+
+        suitHash += SUITBIT_BY_ID[a];
+        suitHash += SUITBIT_BY_ID[b];
+        suitHash += SUITBIT_BY_ID[c];
+        suitHash += SUITBIT_BY_ID[d];
+        suitHash += SUITBIT_BY_ID[e];
+        suitHash += SUITBIT_BY_ID[f];
+        suitHash += SUITBIT_BY_ID[g];
+
+        if (SUITS[suitHash] > 0) {
+            final int[] suitBinary = new int[4]; // add constant for NUMERAL?
+            suitBinary[a & 0x3] |= BINARIES_BY_ID[a];
+            suitBinary[b & 0x3] |= BINARIES_BY_ID[b];
+            suitBinary[c & 0x3] |= BINARIES_BY_ID[c];
+            suitBinary[d & 0x3] |= BINARIES_BY_ID[d];
+            suitBinary[e & 0x3] |= BINARIES_BY_ID[e];
+            suitBinary[f & 0x3] |= BINARIES_BY_ID[f];
+            suitBinary[g & 0x3] |= BINARIES_BY_ID[g];
+
+            return FLUSH[suitBinary[SUITS[suitHash] - 1]];
+        }
+
+        final byte[] quinary = new byte[13]; // add constant for NUMERAL?
+
+        quinary[a >> 2]++;
+        quinary[b >> 2]++;
+        quinary[c >> 2]++;
+        quinary[d >> 2]++;
+        quinary[e >> 2]++;
+        quinary[f >> 2]++;
+        quinary[g >> 2]++;
+
+        final int hash = hashQuinary(quinary, 7);
+
+        return NOFLUSH7[hash];
+    }
+
+    private static int hashQuinary(final byte[] q, int k) {
+        int sum = 0;
+        final int len = 13;
+
+        for (int i = 0; i < len; ++i) {
+            sum += DP[q[i]][len-i-1][k];
+
+            k -= q[i];
+
+            if (k <= 0) {
+                break;
+            }
+        }
+        return sum;
+    }
+
+    private static final short[] NOFLUSH7;//Hashtable7.NOFLUSH7;
+
+    static {
+        NOFLUSH7 = new short[49205];
+        try (DataInputStream inputStream = new DataInputStream(Objects.requireNonNull(HandEvaluator.class.getClassLoader().getResourceAsStream("noflush7.dat")))) {
+            int length = inputStream.available() / Short.BYTES;
+            for (int i = 0; i < length; i++) {
+                NOFLUSH7[i] = inputStream.readShort();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // placeholder for now
+    private static final int[][][] DP = DPTables.DP;
+
+    // placeholder for now
+    private static final short[] FLUSH = HashtableFlush.FLUSH;
+
+    // placeholder for now
+    private static final byte[] SUITS = DPTables.SUITS;
+
+    private static final short[] BINARIES_BY_ID = {
+            0x1,    0x1,    0x1,    0x1,
+            0x2,    0x2,    0x2,    0x2,
+            0x4,    0x4,    0x4,    0x4,
+            0x8,    0x8,    0x8,    0x8,
+            0x10,   0x10,   0x10,   0x10,
+            0x20,   0x20,   0x20,   0x20,
+            0x40,   0x40,   0x40,   0x40,
+            0x80,   0x80,   0x80,   0x80,
+            0x100,  0x100,  0x100,  0x100,
+            0x200,  0x200,  0x200,  0x200,
+            0x400,  0x400,  0x400,  0x400,
+            0x800,  0x800,  0x800,  0x800,
+            0x1000, 0x1000, 0x1000, 0x1000,
+    };
+
+    private static final short[] SUITBIT_BY_ID = {
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+            0x1, 0x8, 0x40, 0x200,
+    };
+}
